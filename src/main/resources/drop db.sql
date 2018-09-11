de8c26649b83691e19c1bd1b8916d0c0ae1b041f
KameKAgZZe <kamekagzze@mail.ru>
2018-09-12 03:45:37 +0600

    Update drop db.sql
    update sql-requst

--- src/main/resources/drop db.sql
+++ src/main/resources/drop db.sql
@@ -1,6 +1,16 @@
 DROP TABLE user_roles;
+DROP TABLE booking_place;
+DROP TABLE booking;
 DROP TABLE users;
 DROP TABLE roles;
 DROP TABLE movie_genres;
+DROP TABLE session;
 DROP TABLE movies;
 DROP TABLE genres;
+DROP TABLE row;
+DROP TABLE room;
+
+
+
+
+
